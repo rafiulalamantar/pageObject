3e3eee074c12f103c8d5cc96789fb2d0d6b76e06
Rafiul/ITP/Md. Rafiul Alam (Email: rafiul.alam@reddotdigitalit.com) <rafiul.alam@reddotdigitalit.com>
2023-09-02 20:22:48 +0600

    Add Action to home page

--- src/main/java/com/qa/crm/pages/HomePage.java
+++ src/main/java/com/qa/crm/pages/HomePage.java
@@ -3,12 +3,27 @@ package com.qa.crm.pages;
 import com.qa.crm.base.TestBase;
 import org.openqa.selenium.support.PageFactory;
 
+<<<<<<< HEAD
+=======
+import java.util.concurrent.TimeUnit;
+
+>>>>>>> cefc44d (Add Action to home page)
 public class HomePage extends TestBase {
 
 
 
     public HomePage(){
         PageFactory.initElements(driver, this);
+<<<<<<< HEAD
+=======
+        TestBase.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    /** Actions are hare */
+
+    public String pageTitle(){
+        return driver.getTitle();
+>>>>>>> cefc44d (Add Action to home page)
     }
 
 }
